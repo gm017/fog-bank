98de03895f588558d970528819450c0029d0a1e0
gabriel-5 <gmanz001@gold.ac.uk>
2023-04-15 12:08:33 +0100

    add comments and delete unused sounds

--- sketch.js
+++ sketch.js
@@ -8,6 +8,8 @@ INSTRUCTIONS:
 
 Start it up by pressing 'p'. The drones will play on for a while. If they stop you can press 'p' again.
 
+Click the 'GET MICROPHNONE' button to give the broser access to the mic.
+
 QWERTY keys toggle "live mode" for each tone. When these are active you can control the pitch of the tone with your voice (through a microphone). 
 Once set, the frequency is locked in if you make it unlive until you change to a different preset (explained below).
 You can see that it's in "live mode" if the texture on the cube changes from the letter to fire.
@@ -23,17 +25,23 @@ Pitch detection stuff is all taken from https://github.com/cwilso/PitchDetect. T
 
 */
 
+// let pitchDisplay;
 
-let pitchDisplay;
-
-let button1;
-
+//Toggles on when key 'p' is pressed so we know we're on
 let playing = false;
-let osc;
-let osc2;
-let lfo;
 
+//Decalare varibales for objects.
+let button1;
+let button2;
+let button3;
+let button4;
+let button5;
+let button6;
+
+let dist;
+let delay;
 let synth;
+
 let fmSynth1;
 let fmSynth2;
 let fmSynth3;
@@ -41,6 +49,14 @@ let fmSynth4;
 let fmSynth5;
 let fmSynth6;
 
+let presetDisplay1;
+let presetDisplay2;
+let presetDisplay3;
+let presetDisplay4;
+let presetDisplay5;
+let presetDisplay6
+
+//Declare varirables for texture images.
 let fire;
 let cloud;
 let compass;
@@ -51,16 +67,18 @@ let rImg;
 let tImg;
 let yImg;
 
+//Decalre variables 
+let screenRotateCount = 0;
+let makeRotation = false;
+let makeRotation2 = false;
+
+//Controls the intensity of the distortion. 
 let distCount = 3;
 
+//Declare varibales related to preset settings.
 let presetCount = 0;
 let presetsArray = [];
-let dist;
-let delay;
-
 let presetBeat = false;
-
-let pitchArray = [];
 
 function preload() {
   fire = loadImage('img/fire.jpeg');
@@ -74,14 +92,13 @@ function preload() {
   yImg = loadImage('img/y.png');
 }
 
-function setup() { //BEGIN SETUP
+function setup() {
   createCanvas(600, 400, WEBGL);
   background(255, 100, 100);
 
+  //Tone.js stuff. Creates synths and effects and sends them to the master.
   dist = new Tone.Chebyshev(distCount);
   delay = new Tone.FeedbackDelay("4n", 0.2);
-
-  // lfo = new Tone.LFO("5hz", 200, 400).start();
 
   fmSynth1 = new Tone.FMSynth();
   fmSynth2 = new Tone.FMSynth();
@@ -97,6 +114,7 @@ function setup() { //BEGIN SETUP
   fmSynth5.chain(dist, Tone.Master);
   fmSynth6.chain(dist, Tone.Master);
 
+  //Create buttons at the top with the floating cubes from the SynthButton class. These represent the six tones playing.
   button1 = new SynthButton(0, 0, 100, qImg, false, true);
   button2 = new SynthButton(100, 0, 100, wImg, false, true);
   button3 = new SynthButton(200, 0, 100, eImg, false, true);
@@ -104,6 +122,7 @@ function setup() { //BEGIN SETUP
   button5 = new SynthButton(400, 0, 100, tImg, false, true);
   button6 = new SynthButton(500, 0, 100, yImg, false, true);
 
+  //Create displays from the PresetDisplays class showing which preset is currently selected.
   presetDisplay1 = new PresetDisplays(0, 100, 100, 255, false, 0);
   presetDisplay2 = new PresetDisplays(100, 100, 100, 255, false, 1);
   presetDisplay3 = new PresetDisplays(200, 100, 100, 255, false, 2);
@@ -111,6 +130,7 @@ function setup() { //BEGIN SETUP
   presetDisplay5 = new PresetDisplays(400, 100, 100, 255, false, 4);
   presetDisplay6 = new PresetDisplays(500, 100, 100, 255, false, 5);
 
+  //PresetValues class is used to create objects which contain the values for the six preset settings.
   presetValues1 = new PresetValues(40, 41, 42, 193, 194, 95);
   presetValues2 = new PresetValues(101, 105, 109, 401, 405, 1223);
   presetValues3 = new PresetValues(200, 202, 209, 601, 605, 1211);
@@ -118,17 +138,24 @@ function setup() { //BEGIN SETUP
   presetValues5 = new PresetValues(401, 405, 409, 1001, 1005, 1277);
   presetValues6 = new PresetValues(501, 505, 509, 1201, 1205, 1277);
 
+  //Array contains the objects created previously.
   presetsArray = [presetValues1, presetValues2, presetValues3, presetValues4, presetValues5, presetValues6];
 
+  //Object from the DelayConnectors class which shows whether the delay effect is connected to the synths with the floating cloud globe.
   delayConnector = new DelayConnectors(0, 50, 25);
+
+  //Object from teh DistConnectors class which shows whether the distortion effect is connected to the synths with the floating compass globe.
   distConnector = new DistConnectors(0, 200, 25);
 
 }
 
 function draw() { //BEGIN DRAW
   background(255, 100, 100);
+
+  //Functions to occur during the draw loop.
+  rotateScreenRight();
+  rotateScreenLeft();
   voiceControlPitch();
-  increaseDistortion();
   displayButtons();
   delayConnector.display();
   distConnector.display();
@@ -144,22 +171,22 @@ function draw() { //BEGIN DRAW
   // box(50)
   // pop();
 
-  if (presetBeat) {
-    if (presetCount < 5) {
-      if (frameCount % 30 === 0) {
-        presetCount++;
-        switchPreset(presetsArray[presetCount]);
-      }
-    } else if (frameCount % 30 === 0 && presetCount === 5) {
-      presetCount = 0;
-    }
-  }
+  // if (presetBeat) {
+  //   if (presetCount < 5) {
+  //     if (frameCount % 30 === 0) {
+  //       presetCount++;
+  //       switchPreset(presetsArray[presetCount]);
+  //     }
+  //   } else if (frameCount % 30 === 0 && presetCount === 5) {
+  //     presetCount = 0;
+  //   }
+  // }
 }
 
-
+//Takes the pitch from the users microphone and uses it to alter the frequencies of the synths. 
+//The functionality of the pitch detection is from https://github.com/cwilso/PitchDetect and can be found in 'js/pitchdetect.js'
 function voiceControlPitch() {
   if (playing) {
-    // Audio stuff
     if (typeof pitch === 'number' && pitch < 1000) {
       if (button1.live === 1) {
         fmSynth1.frequency.value = pitch + 1000;
@@ -215,19 +242,7 @@ function voiceControlPitch() {
   }
 }
 
-function displayPresets() {
-  push();
-  translate(-width / 2, -height / 2, 0);
-  noStroke();
-  presetDisplay1.display();
-  presetDisplay2.display();
-  presetDisplay3.display();
-  presetDisplay4.display();
-  presetDisplay5.display();
-  presetDisplay6.display();
-  pop();
-}
-
+//Displays the floating cubes which represent each of the six tones playing. (Row 1)
 function displayButtons() {
   push();
   translate(-width / 2, -height / 2, 0);
@@ -241,14 +256,31 @@ function displayButtons() {
   pop();
 }
 
-function increaseDistortion() {
-  if (distCount < 100) {
-    distCount += 0.1;
-  } else {
-    distCount = 3;
-  }
+//Displays the preset selection indicator. The selected preset is represented by the yellow square with moving red lines. (Row 2)
+//They can be cycled through with the left and right arrow keys.
+function displayPresets() {
+  push();
+  translate(-width / 2, -height / 2, 0);
+  noStroke();
+  presetDisplay1.display();
+  presetDisplay2.display();
+  presetDisplay3.display();
+  presetDisplay4.display();
+  presetDisplay5.display();
+  presetDisplay6.display();
+  pop();
 }
 
+//NEED TO FIX
+// function increaseDistortion() {
+//   if (distCount < 100) {
+//     distCount += 0.1;
+//   } else {
+//     distCount = 3;
+//   }
+// }
+
+//When the user changes to a new preset, this assigns new frequency values to the synths from presetsArray, which contains the objects with the values.
 function switchPreset(vals) {
   fmSynth1.frequency.value = vals.synth1Freq;
   fmSynth2.frequency.value = vals.synth2Freq;
@@ -258,13 +290,44 @@ function switchPreset(vals) {
   fmSynth6.frequency.value = vals.synth6Freq;
 }
 
-function keyPressed() {
+//Rotates the display 360 degrees anti-clockwise when the preset selection loops round from the right side to the left side.
+function rotateScreenRight() {
+  if (makeRotation === true) {
+    screenRotateCount += 6;
+    if (screenRotateCount < 720) {
+      screenRotateCount += 2;
+      rotateY(radians(screenRotateCount));
+    }
+    if (screenRotateCount === 360) {
+      screenRotateCount = 0;
+      makeRotation = false;
+    }
+  }
+}
 
+//Rotates the display 360 degrees clockwise when the preset selection loops round from the left side to the right side.
+function rotateScreenLeft() {
+  if (makeRotation2 === true) {
+    screenRotateCount -= 6;
+    if (screenRotateCount > -360) {
+      screenRotateCount -= 2;
+      rotateY(radians(screenRotateCount));
+    }
+    if (screenRotateCount === -360) {
+      screenRotateCount = 0;
+      makeRotation2 = false;
+    }
+  }
+}
+
+function keyPressed() {
+  //Cycles through the presets with the right and left arrow keys. This also controls the screen rotation if the user loops from one edge to the other.
   if (keyCode === RIGHT_ARROW && presetCount < 5) {
     presetCount++
     switchPreset(presetsArray[presetCount]);
   } else if (keyCode === RIGHT_ARROW && presetCount === 5) {
     presetCount = 0;
+    makeRotation = true;
     switchPreset(presetsArray[presetCount]);
   }
   if (keyCode === LEFT_ARROW && presetCount > 0) {
@@ -272,6 +335,7 @@ function keyPressed() {
     switchPreset(presetsArray[presetCount]);
   } else if (keyCode === LEFT_ARROW && presetCount === 0) {
     presetCount = 5;
+    makeRotation2 = true;
     switchPreset(presetsArray[presetCount]);
   }
 
@@ -279,7 +343,7 @@ function keyPressed() {
     playing = true;
   }
 
-
+  //Pressing 'p' begins the sound with initial settings.
   if (key === 'p') {
     fmSynth1.triggerAttackRelease(40, 1200);
     fmSynth2.triggerAttackRelease(41, 1200);
@@ -289,6 +353,9 @@ function keyPressed() {
     fmSynth6.triggerAttackRelease(95, 1200);
   }
 
+
+  //QWERTY keys toggle wether each synth is in 'live mode'. When this is active, the frequency of the tone is controlled by the users voice. 
+  //This changes a value in the button objects and also changes their appearance to indicate that the synth is in 'live mode'.
   if (key === 'q') {
     if (button1.live === 0) {
       button1.live = 1;
@@ -332,6 +399,8 @@ function keyPressed() {
     }
   }
 
+  //Pressing the 'a' key toggles the synths being connected to the delay effect. 
+  //This changes a value in the button objects, which tells the delayConnector object to draw a line between the two showing that it is active.
   if (key === 'a') {
     if (button1.delayConnected) {
       button1.delayConnected = false;
@@ -424,11 +493,12 @@ function keyPressed() {
 
 function mouseClicked() {
 
+  //NEED TO FIX/ALTER/DELTE
+  // if (mouseY > 300) {
+  //   presetBeat = true;
+  // }
 
-  if (mouseY > 300) {
-    presetBeat = true;
-  }
-
+  //Mouse clicks can be used to toggle 'live mode' on the synths as an alternative to using the QWERTY keys.
   if (mouseY > 0 && mouseY < 100) {
     if (mouseX < 100) {
       if (button1.live === 0) {
